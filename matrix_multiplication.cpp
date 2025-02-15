e0fb72bcb454f79e2fd039c0232c3c5d21c9d218
Anish-2005 <anishseth0510@gmail.com>
2025-02-15 20:38:45 +0530

    Create matrix_multiplication.cpp

---
+++ matrix_multiplication.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+
+using namespace std;
+
+void multiplyMatrices(int A[][10], int B[][10], int result[][10], int r1, int c1, int c2) {
+    for (int i = 0; i < r1; i++) {
+        for (int j = 0; j < c2; j++) {
+            result[i][j] = 0;
+            for (int k = 0; k < c1; k++) {
+                result[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
+}
+
+void printMatrix(int matrix[][10], int rows, int cols) {
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    int r1, c1, r2, c2;
+    cout << "Enter rows and columns of first matrix: ";
+    cin >> r1 >> c1;
+    cout << "Enter rows and columns of second matrix: ";
+    cin >> r2 >> c2;
+
+    if (c1 != r2) {
+        cout << "Matrix multiplication not possible. Columns of first matrix must be equal to rows of second matrix.\n";
+        return 0;
+    }
+
+    int A[10][10], B[10][10], result[10][10];
+
+    cout << "Enter elements of first matrix:\n";
+    for (int i = 0; i < r1; i++) {
+        for (int j = 0; j < c1; j++) {
+            cin >> A[i][j];
+        }
+    }
+
+    cout << "Enter elements of second matrix:\n";
+    for (int i = 0; i < r2; i++) {
+        for (int j = 0; j < c2; j++) {
+            cin >> B[i][j];
+        }
+    }
+
+    multiplyMatrices(A, B, result, r1, c1, c2);
+
+    cout << "\nResultant Matrix:\n";
+    printMatrix(result, r1, c2);
+
+    return 0;
+}
