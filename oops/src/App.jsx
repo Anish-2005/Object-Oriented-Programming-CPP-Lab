705d866b3cf5b63c011930b3dea0626d5a5230e3
Anish-2005 <anishseth0510@gmail.com>
2025-04-17 02:49:16 +0530

    Update App.jsx

--- oops/src/App.jsx
+++ oops/src/App.jsx
@@ -1,7 +1,8 @@
 // src/App.jsx
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Landing from './pages/Landing';
-import Labs from './pages/Lab';
+import Lab from './pages/Lab';
+import Labs from './pages/Labs';
 import AddLab from './pages/AddLab';
 import Compiler from './pages/Complier';
 import Learn from './pages/Learn';
@@ -11,7 +12,8 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Landing />} />
-        <Route path="/labs" element={<Labs />} />
+        <Route path="/labs" element={<Lab />} />
+        <Route path="/lab" element={<Labs />} />
         <Route path="/add" element={<AddLab />} />
         <Route path="/compile" element={<Compiler />} />
         <Route path="/learn" element={<Learn />} />
